08e0d56ffbf72016e1af60e3c5ff61568a640e5e
Yue JIN <yjin@nustarnuclear.com>
2024-01-02 17:46:49 +0800

    feat: upgrade to vite 5 and electron 28

--- electron/main.ts
+++ electron/main.ts
@@ -1,5 +1,9 @@
 import { app, BrowserWindow, ipcMain } from 'electron'
-import { join } from 'node:path'
+import { join, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
 
